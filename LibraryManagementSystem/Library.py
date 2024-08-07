fc216b94998424810cd559af284aa2b44f6065e2
Shazain PC <shazain_sq@hotmail.com>
2024-08-08 00:51:18 +0500

    new package for LMS

--- LibraryManagementSystem/Library.py
+++ LibraryManagementSystem/Library.py
@@ -9,9 +9,14 @@ class Library:
 
     def add_Book(self, title, author):
         book = Book(title, author)
+        self.books.append(book)
 
+    # def __validate__(self, book):
+    #     if not isinstance(book, Book):
+    #         raise TypeError("__validate__: Incorrect object type")
 
-    def __validate__(self, book):
-        if not isinstance(book, Book):
-            raise TypeError("__validate__: Incorrect object type")
-
+    def remove_Book(self, title, author):
+        for book in self.books:
+            if book.title == title and book.author == author:
+                temp_Book = book
+                self.books.
