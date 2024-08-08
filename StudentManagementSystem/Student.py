96e5c87adb70914ec0bf778341d503f6623b3494
Shazain PC <shazain_sq@hotmail.com>
2024-08-09 01:51:15 +0500

    Student.py type check and None check added

--- StudentManagementSystem/Student.py
+++ StudentManagementSystem/Student.py
@@ -3,14 +3,23 @@ class Student:
     _id = 0
     _major = ""
 
+    # Sets the attributes if type check passes and vars not empty
     def __init__(self, name: str, id: int, major: str):
-        self._name = name
-        self._id = id
-        self.major = major
+        # if name is None or id is None or major is None:
+        #     print("Invalid Attributes")
+        #     return
+
+        if isinstance(name, str) and not name is None:
+            self._name = name
+        if isinstance(id, int) and id is not None:      # Can do both / 'not' before and in bw
+            self._id = id
+        if isinstance(major, str) and major is not None:
+            self.major = major
 
         # Changes the major to the input
-    def update_major(self, new_major: str):
-        self.major = new_major
+    def update_major(self, major: str):
+        if isinstance(major, str) and major is not None:
+            self.major = major
 
     def __str__(self):
         return f"{self._name}, {self._id}, {self.major}"
